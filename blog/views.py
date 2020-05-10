99d8be64bdf1e83a1df6a69520709db5cc260e7d
Jay Desai <jaydesai571@gmail.com>
2020-05-10 19:14:53 -0400

    Post creation using class method instead of function method

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Post
 
@@ -7,6 +8,11 @@ def home(request):
 		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	models = Post
+	template_name = 'blog/home.html' #<app>
+	context_object_name = 'posts' 
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'about'})
