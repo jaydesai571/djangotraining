0ead89d5e90f2165cc206fdb21e72dfb8393d137
Jay Desai <jaydesai571@gmail.com>
2020-05-11 02:19:06 -0400

    Complete User management system with blog CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 	ListView, 
 	DetailView, 
 	CreateView,
-	UpdateView
+	UpdateView,
+	DeleteView
 	)
 from django.http import HttpResponse
 from .models import Post
@@ -24,6 +25,16 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
 	model = Post
 
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+
+	success_url = '/'
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
 class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	fields = ['title', 'content']
@@ -32,13 +43,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
 	fields = ['title', 'content']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'about'})
