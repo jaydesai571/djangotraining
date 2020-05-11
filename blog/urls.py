1e4c4b9d5b00ead5c78a542e3b185273175e2641
Jay Desai <jaydesai571@gmail.com>
2020-05-10 20:14:24 -0400

    Blog cretion, updating, detail view created

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+	PostListView, 
+	PostDetailView, 
+	PostCreateView,
+	PostUpdateView
+	)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view() , name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update')
 ]
